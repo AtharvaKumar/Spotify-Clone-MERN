dd5556ca2bfc6bfd9d651296e2a2153f5265fa93
Atharva Kumar <iamatharvakumar@gmail.com>
2024-07-14 17:38:19 +0530

    Connect vercel

--- spotify-backend/server.js
+++ spotify-backend/server.js
@@ -5,6 +5,7 @@ import 'dotenv/config'
 import connectDB from "./src/config/mongodb.js";
 import songRouter from "./src/routes/songRoute.js";
 import albumRouter from "./src/routes/albumRoute.js";
+import mongoose, { Mongoose } from "mongoose";
 
 // app config
 const app = express()
@@ -15,6 +16,8 @@ connectDB()
 // middlewares
 app.use(express.json())
 app.use(cors())
+
+mongoose.connect("mongodb+srv://mern-book-store:Honuman%401928@cluster0.btbrnal.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 // Initializing Routers
 app.use("/api/song", songRouter )
